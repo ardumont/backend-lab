cd34a36e59bea0cddc641b836a75ec21150813a4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-30 02:57:43 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mysql-to-datomic
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/mysql_to_datomic/core_test.clj
@@ -0,0 +1,7 @@
+(ns mysql-to-datomic.core-test
+  (:use clojure.test
+        mysql-to-datomic.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
