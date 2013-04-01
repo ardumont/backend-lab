56ed1c06ed5c7568e92c9aa84e28691c8dc2df24
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-01 19:09:15 +0200

    Adding skills (beware, there seems to be some hole in tempid). A very good lesson (this may be obvious to others), when using tempid, of course, this must be inside the same transaction!

--- src/backend_lab/datomic.clj
+++ src/backend_lab/datomic.clj
@@ -1,7 +1,7 @@
 (ns backend-lab.datomic
   "Play with datomic"
-  (:use [datomic.api :only [q db] :as d]
-        [clojure.pprint]))
+  (:use [datomic.api :only [q db] :as d])
+  (:require [clojure.pprint :as p]))
 
 ;; store database uri
 (def uri "datomic:mem://eleves")
@@ -19,66 +19,64 @@
 (def schema-tx (-> "samples/eleves-schema.dtm" slurp read-string))
 
 @(d/transact conn schema-tx)
-;; {:db-before datomic.db.Db@6a643996, :db-after datomic.db.Db@18cd6827, :tx-data [#Datum{:e 13194139534316 :a 50 :v #inst "2013-03-31T17:38:40.417-00:00" :tx 13194139534316 :added true} #Datum{:e 0 :a 13 :v 62 :tx 13194139534316 :added true} #Datum{:e 0 :a 13 :v 63 :tx 13194139534316 :added true} #Datum{:e 0 :a 13 :v 64 :tx 13194139534316 :added true}], :tempids {-9223350046623220293 17592186045419, -9223350046623220292 17592186045418, -9223350046623220291 17592186045417, -9223367638809264706 64, -9223367638809264705 63, -9223367638809264704 62}}
 
 (def data-tx (-> "samples/eleves-data.dtm" slurp read-string))
 
 @(d/transact conn data-tx)
 
 ;; persons
-(pprint "person query")
-(pprint (q '[:find ?c ?fn ?n ?profile
-             :where
-             [?c :person/firstname ?fn]
-             [?c :person/name ?n]
-             [?c :person/profile ?p]
-             [?p :db/ident ?profile]] (db conn)))
+(p/pprint "person query")
+(p/pprint (q '[:find ?c ?fn ?n ?profile
+               :where
+               [?c :person/firstname ?fn]
+               [?c :person/name ?n]
+               [?c :person/profile ?p]
+               [?p :db/ident ?profile]] (db conn)))
 
 ;; schools
-(pprint "school query")
-(pprint (q '[:find ?c ?sn ?st ?sz
-             :where
-             [?c :school/name ?sn]
-             [?c :school/town ?st]
-             [?c :school/zipcode ?sz]]
-           (db conn)))
+(p/pprint "school query")
+(p/pprint (q '[:find ?c ?sn ?st ?sz
+               :where
+               [?c :school/name ?sn]
+               [?c :school/town ?st]
+               [?c :school/zipcode ?sz]]
+             (db conn)))
 
 ;; classe
-(pprint "classe query - 1")
-(pprint (q '[:find ?c ?n ?y ?sn
-             :where
-             [?c :classe/name ?n]
-             [?c :classe/year ?y]
-             [?c :classe/school ?i]
-             [?i :school/name ?sn]]
-           (db conn)))
+(p/pprint "classe query - 1")
+(p/pprint (q '[:find ?c ?n ?y ?sn
+               :where
+               [?c :classe/name ?n]
+               [?c :classe/year ?y]
+               [?c :classe/school ?i]
+               [?i :school/name ?sn]]
+             (db conn)))
 
-(pprint "classe query - 2")
-(pprint (q '[:find ?n ?y ?tn ?tfn
-             :where
-             [?c :classe/name ?n]
-             [?c :classe/year ?y]
-             [?c :classe/teachers ?t]
-             [?t :person/name ?tn]
-             [?t :person/firstname ?tfn]]
-           (db conn)))
+(p/pprint "classe query - 2")
+(p/pprint (q '[:find ?n ?y ?tn ?tfn
+               :where
+               [?c :classe/name ?n]
+               [?c :classe/year ?y]
+               [?c :classe/teachers ?t]
+               [?t :person/name ?tn]
+               [?t :person/firstname ?tfn]]
+             (db conn)))
 
-(pprint "cycle query")
-(pprint (q '[:find ?cn
-             :where
-             [?c :cycle/name ?cn]]
-           (db conn)))
+(p/pprint "cycle query")
+(p/pprint (q '[:find ?cn
+               :where
+               [?c :cycle/name ?cn]]
+             (db conn)))
 
 ;; (->> (select domaines)
 ;;      (map (fn [{:keys [id_cycle domaine_nom]}]
 ;;             (let [m {1 -1000007 2 -1000008 3 -1000009}
 ;;                   cid (m id_cycle)]
-;;               {:db/id (str "#db/id[:db.part/user " -1000010 "]")
+;;               {:db/id (load-string (str "#db/id[:db.part/user " -1000010 "]"))
 ;;                :domain/name domaine_nom
-;;                :domain/cycle (str "#db/id[:db.part/user " cid "]")}))))
+;;                :domain/cycle (load-string (str "#db/id[:db.part/user " cid "]"))}))))
 
-
-(pprint (q '[:find ?dn ?cn
+(p/pprint (q '[:find ?dn ?cn
              :where
              [?c :domain/name ?dn]
              [?c :domain/cycle ?cc]
@@ -89,13 +87,31 @@
 ;;      (map (fn [{:keys [id_domaine matiere_nom matiere_id]}]
 ;;             (let [m (zipmap (range 1 19) (map (partial * -1) (range 1000010 1000028)))
 ;;                   id (m id_domaine)]
-;;               {:db/id (str "#db/id[:db.part/user " (- -1000028 matiere_id) "]")
+;;               {:db/id (load-string (str "#db/id[:db.part/user " (- -1000027 matiere_id) "]"))
 ;;                :subject/name matiere_nom
-;;                :subject/domain (str "#db/id[:db.part/user " id "]")}))))
+;;                :subject/domain (load-string (str "#db/id[:db.part/user " id "]"))}))))
 
-(pprint (q '[:find ?sn ?dn
+(p/pprint (q '[:find ?sn ?dn
              :where
              [?c :subject/name ?sn]
              [?c :subject/domain ?d]
              [?d :domain/name ?dn]]
            (db conn)))
+
+;; (->> (select competences)
+;;      (map (fn [{:keys [id_matiere competence_nom competence_id]}]
+;;             (let [m (->> (range 1000028 1000107)
+;;                          (map (partial * -1))
+;;                          (zipmap (range 1 79)))
+;;                   id (m id_matiere)]
+;;               {:db/id (load-string (str "#db/id[:db.part/user " (- -1000107 competence_id) "]"))
+;;                :skill/name competence_nom
+;;                :skill/subject (load-string (str "#db/id[:db.part/user " id "]"))}))))
+
+(p/pprint "query skills")
+(p/pprint (q '[:find ?sn ?subn
+               :where
+               [?c :skill/name ?sn]
+               [?c :skill/subject ?ss]
+               [?ss :subject/name ?subn]]
+             (db conn)))
